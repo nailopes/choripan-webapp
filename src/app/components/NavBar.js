7a62eb57f60536465297aab01bff1e21c08fdd7c
Naiara <naiaralopess@gmail.com>
2025-04-05 23:22:04 -0600

    navbar modification

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -50,14 +50,14 @@ const Navbar = () => {
 
                 {/* Social Media (Left Side - Hidden on Small Screens, Visible on Tablets and Larger) */}
                 <div className="hidden md:flex space-x-4">
-                    <a
+                    {/* <a
                         href="https://www.facebook.com"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="w-12 h-12 flex items-center justify-center rounded-full bg-accent-orange hover:bg-white hover:text-black transition-colors"
                     >
                         <SiFacebook size={28} />
-                    </a>
+                    </a> */}
                     <a
                         href="https://www.instagram.com"
                         target="_blank"
@@ -66,55 +66,25 @@ const Navbar = () => {
                     >
                         <SiInstagram size={28} />
                     </a>
-                    <a
+                    {/* <a
                         href="https://x.com"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="w-12 h-12 flex items-center justify-center rounded-full bg-accent-orange hover:bg-white hover:text-black transition-colors"
                     >
                         <SiX size={28} />
-                    </a>
+                    </a> */}
                 </div>
 
                 {/* Navigation Button & Dropdown (Always viseble)*/}
-                <div className="relative ml-auto" ref={dropdownRef}>
-                    {/* Button to Open Dropdown */}
-                    <button
-                        onClick={() => setIsOpen(!isOpen)}
-                        className="
-                          flex items-center justify-center
-                         border-2 border-main-red
-                         hover:bg-white hover:text-main-red
-                         bg-main-red text-white transition
-                         rounded-lg
+                <div className="relative ml-auto">
+                    <Link href="/" className='font-bondrians text-xl px-5'>Home
+                    </Link>
+                    <Link href="/about" className='font-bondrians text-xl px-5'>About Us
+                    </Link>
+                    <Link href="/contact" className='font-bondrians text-xl px-5'>Contact
+                    </Link>
 
-                         w-12 h-12                   /* Square on small screens */
-                         md:w-32 md:h-12             /* medium rectangle on md screens */
-                         lg:w-48 lg:px-6 lg:py-3     /* larger button with padding on large+  */">
-                        <Menu size={20} className="sm:size-5 md:size-6" />
-                        <span className="font-semibold hidden lg:inline text-base ml-2">
-                            Navigation
-                        </span>
-                    </button>
-
-                    {/* Dropdown Menu (Appears Below Button) */}
-                    {isOpen && (
-                        <div className="absolute right-0 md:w-full bg-white shadow-lg rounded-lg py-3 top-full mt-2 border border-gray-200">
-                            <ul className="flex flex-col items-center space-y-3 text-lg font-semibold">
-                                {navItems.map((item) => (
-                                    <li key={item.name}>
-                                        <Link
-                                            href={item.href}
-                                            className="block w-full text-center px-4 py-2 hover:bg-gray-100 rounded transition"
-                                            onClick={() => setIsOpen(false)}
-                                        >
-                                            {item.name}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
                 </div>
             </div>
         </nav>
